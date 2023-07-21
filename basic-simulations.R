ebbd847bfba20dc356e13711e7ec3deac3ad67ff
Katie Millar <ajh20fhu@uea.ac.uk>
2023-07-21 19:26:46 +0100

    practising some basic simulations

---
+++ basic-simulations.R
@@ -0,0 +1,16 @@
+# practising some random selection 
+
+# random number generator is 1 
+set.seed(1)
+
+# using the sample function 
+sample(1:10, 4)
+# this wll select 4 numbers from 1-10
+
+# using this for letters 
+sample(letters, 4)
+# this will select 4 random letters
+
+sample(1:10)
+# simple code, will just select 10 numbers 1-10 in a random order 
+
