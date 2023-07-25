908f30f670ac38b55e77b03d48fe6e449e459a99
Katie Millar <ajh20fhu@uea.ac.uk>
2023-07-25 15:39:28 +0100

    practising some basic simulations

--- basic-simulations.R
+++ basic-simulations.R
@@ -20,6 +20,15 @@ sample(1:10)
 sample(1:10, replace = TRUE)
 # this means there will be repeats of numbers
 
-# USING DATA SETS ---- 
-library(datasets)
+# simulating normal random numbers
+# using mean of 0, and a standard deviation of 1 
+x <- rnorm(10)
+# 10 random numbers
 
+
+# setting a mean and sd 
+x <- rnorm(10, 20, 2)
+# setting a mean of 20 and an sd of 2 
+
+# getting some statistical values 
+summary(x)
