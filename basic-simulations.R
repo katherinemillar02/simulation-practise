526e115648cf3ac8c361ffb6cbd6fc8a812b918a
Katie Millar <ajh20fhu@uea.ac.uk>
2023-07-27 10:55:16 +0100

    working on binomial distribution simulations

--- basic-simulations.R
+++ basic-simulations.R
@@ -76,3 +76,19 @@ y <- 0.5 + 2 * x + e
 summary(y)
 
 plot(x, y)
+
+
+
+
+# binomial distribution 
+set.seed(10)
+
+x <- rbinom(100, 1, 0.5) # n, size, prob
+
+str(x) # structure of R objects 
+
+e <- rnorm(100, 0, 2)
+
+y <- 0.5 + 2 * x + e 
+
+
