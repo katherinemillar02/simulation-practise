0564446d07558cf133fd18379c2c2eb59337dcd6
Katie Millar <ajh20fhu@uea.ac.uk>
2023-07-30 15:50:49 +0100

    adding comments to the if function simulations

--- miami_simulations_chapter.R
+++ miami_simulations_chapter.R
@@ -20,24 +20,43 @@ sample(groups, size = 8, replace = TRUE)
 sample(groups, size = 8) # does not work - as only two elements to choose from 
 
 # using 'if'
-if_test <- function(x) {
+if_test <- function(x) { 
   if (x > 0 ) {
     return("positive")
   }
 }
 
+# this labels the function as if.test
+# then says if x is more than 0 than return a positive
+
+# checking with the number 3 is more than 0
+# will return a positive as x is more than 0 
 if_test(3)
 
+# checking with -3
+# does not return anything because not more than 0 
+if_test(-3)
 
+# using print function just to show it can return something 
 print(if_test(-3))
+# returns null 
 
-
+# trying the if function again this time with more or equal to 0 
 if_test_2 <- function(x) {
   if ( x >= 0) {
     return("positive")
   }
 }
 
+# testing 0
 if_test_2(0)
+# says is positive 
 
-if_test_2(0.1)
+# testing -0.1
+print(if_test_2(-0.1))
+# negative as is less than 0 
+
+
+
+
+
