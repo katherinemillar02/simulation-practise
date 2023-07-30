e346a0434ae7b082481dddcfd4766c892191c52b
Katie Millar <ajh20fhu@uea.ac.uk>
2023-07-30 13:25:54 +0100

    veginning a new simularion chapter

--- miami_simulations_chapter.R
+++ miami_simulations_chapter.R
@@ -1,2 +1,19 @@
 # trying to ensure learning of ?tidyverse? simulations 
 
+# coin as an example 
+coin <- c("heads", "tails")
+
+# selecting one of heads or tails
+sample(coin, size = 1)
+
+# groups as an example 
+groups <- c("kick", "receive")
+
+# selecting one of kick or receive 
+sample(groups, size = 1)
+
+# selecting eight of kick or receive 
+sample(groups, size = 8, replace = TRUE)
+
+
+
